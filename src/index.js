d06dddc2938969e802502dd6626800642a29829a
ANOOP MUKTHI <anoopmukthi2000official@gmail.com>
2024-01-30 00:29:46 +0530

    pizza-menu project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,94 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App() {
+  return (
+    <div>
+      <Header />
+      <Pizza />
+      <Footer currentTime={new Date().toLocaleTimeString()} />
+    </div>
+  );
+}
+
+function Pizza() {
+  return (
+    <div>
+      <img src="pizzas/spinaci.jpg" />
+      <h2>Pizza Spinaci</h2>
+      <h3>Tomato, mozarella, spinach, and ricotta cheese</h3>
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>Welcome to Pizza Hut!!!</h1>;
+}
+
+function Menu() {
+  // return()
+}
+
+function Footer(props) {
+  const [currentTime, setCurrentTime] = useState(0);
+  const [currentState, setCurrentState] = useState("Open");
+  const [currentAccurateTime, setCurrentAccurateTime] = useState("");
+  useEffect(() => {
+    setCurrentTime(new Date().getHours());
+    setCurrentState(currentTime >= 9 && currentTime < 17 ? "open" : "closed");
+    setCurrentAccurateTime(new Date().toLocaleTimeString());
+  }, 1000);
+  return (
+    <footer>
+      It's {currentAccurateTime}.We're currently {currentState}
+    </footer>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
